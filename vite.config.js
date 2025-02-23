8242b878803eeec2976e50aecd3f99fcd81e5834
GabriGuerra <gb.guerra@icloud.com>
2025-02-22 22:08:03 -0300

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: 'https://gabriguerra.github.io/cwb-houseplants/',  // Substitua "NOME-DO-REPOSITORIO" pelo nome do seu repositório no GitHub
-})
